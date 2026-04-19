5788b6a80bd7d85b2dc66b1608ce22116819cb23
dfdgsdfg <dfdgsdfg@gmail.com>
2026-04-19 11:22:28 +0900

    Reduce provider-side unix call decode

--- src/dxmt9/dxmt9unix_service_abi.h
+++ src/dxmt9/dxmt9unix_service_abi.h
@@ -1,17 +1,21 @@
 #pragma once
 
 #include "dxmt9/winemetal.h"
+#include "winemetal/winemetal_thunks.hpp"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 dxmt9_u64 dxmt9unix_service_compile_shader(const WinemetalShaderCompileRequest* request);
+dxmt9_u64 dxmt9unix_service_compile_shader_marshaled(const Dxmt9WinemetalCompileShaderParams* params);
 dxmt9_u64 dxmt9unix_service_shader_source_size(dxmt9_u64 shaderHandle);
 dxmt9_u64 dxmt9unix_service_shader_source_copy(dxmt9_u64 shaderHandle,
                                                char* buffer,
                                                dxmt9_u64 bufferCapacity);
+dxmt9_u64 dxmt9unix_service_shader_source_copy_marshaled(const Dxmt9WinemetalShaderSourceCopyParams* params);
 void dxmt9unix_service_destroy_shader(dxmt9_u64 shaderHandle);
+void dxmt9unix_service_destroy_shader_marshaled(const Dxmt9WinemetalDestroyShaderParams* params);
 
 #ifdef __cplusplus
 }
--- src/dxmt9/dxmt9unix_shader_handlers.cpp
+++ src/dxmt9/dxmt9unix_shader_handlers.cpp
@@ -1,32 +1,12 @@
 #include "dxmt9/dxmt9unix_service_abi.h"
 #include "dxmt9unix_shader_handlers.hpp"
 
-#include "util/unixcall_marshal.hpp"
-
 #include <cstdint>
 
 namespace {
 
 constexpr std::int32_t kStatusSuccess = 0;
 constexpr std::int32_t kStatusInvalidParameter = static_cast<std::int32_t>(0xC000000Du);
-
-WinemetalShaderCompileRequest compileShaderRequestFromParams(
-    const Dxmt9WinemetalCompileShaderParams& params) {
-  return WinemetalShaderCompileRequest{
-      .kind = static_cast<WinemetalShaderKind>(params.kind),
-      .bytecode = dxmt9::util::marshal::decodeNullablePtr<const void>(params.bytecode_ptr),
-      .bytecodeSize = params.bytecode_size,
-      .bytecodeHash = params.bytecode_hash,
-      .variantKey = dxmt9::util::marshal::decodeNullablePtr<const void>(params.variant_key_ptr),
-      .textured = params.textured != 0,
-      .clipPlaneMask = params.clip_plane_mask,
-      .sampleCount = params.sample_count,
-      .alphaTestEnable = params.alpha_test_enable,
-      .alphaTestFunc = params.alpha_test_func,
-      .alphaRef = params.alpha_ref,
-      .fogMode = params.fog_mode,
-  };
-}
 
 }  // namespace
 
@@ -35,8 +15,7 @@ extern "C" NTSTATUS dxmt9_winemetal_compile_shader_unix_call(void* opaque) {
   if (!params) {
     return kStatusInvalidParameter;
   }
-  const auto request = compileShaderRequestFromParams(*params);
-  params->ret = dxmt9unix_service_compile_shader(&request);
+  params->ret = dxmt9unix_service_compile_shader_marshaled(params);
   return kStatusSuccess;
 }
 
@@ -54,14 +33,7 @@ extern "C" NTSTATUS dxmt9_winemetal_shader_source_copy_unix_call(void* opaque) {
   if (!params) {
     return kStatusInvalidParameter;
   }
-  char* destination = dxmt9::util::marshal::decodeCharBuffer(
-      params->buffer_ptr, params->buffer_capacity);
-  if (!destination) {
-    params->bytes_written = 0;
-    return kStatusSuccess;
-  }
-  params->bytes_written = dxmt9unix_service_shader_source_copy(
-      params->shader_handle, destination, params->buffer_capacity);
+  params->bytes_written = dxmt9unix_service_shader_source_copy_marshaled(params);
   return kStatusSuccess;
 }
 
@@ -70,6 +42,6 @@ extern "C" NTSTATUS dxmt9_winemetal_destroy_shader_unix_call(void* opaque) {
   if (!params) {
     return kStatusInvalidParameter;
   }
-  dxmt9unix_service_destroy_shader(params->shader_handle);
+  dxmt9unix_service_destroy_shader_marshaled(params);
   return kStatusSuccess;
 }
